f266f07eb8f094dd0b0a10523a2da7d6527b5061
kayladonovan <kdono062@uottawa.ca>
2019-10-17 00:19:04 -0400

    Second commit android project

--- WalkinClinic/app/src/main/java/com/example/walkinclinicv01/WelcomeWindow.java
+++ WalkinClinic/app/src/main/java/com/example/walkinclinicv01/WelcomeWindow.java
@@ -1,8 +1,9 @@
 package com.example.walkinclinicv01;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelcomeWindow extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class WelcomeWindow extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_window);
+
+        //saving values to display (*need to also store user role*)
+        Intent i1 = getIntent();
+        String displayName = i1.getStringExtra(RegistrationWindow.NAME);
+        TextView textView1 = (TextView) findViewById(R.id.firstName);
+
+        textView1.setText(displayName);
     }
+
+
 }
