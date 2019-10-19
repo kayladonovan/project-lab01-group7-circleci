56f6f563161eda9568a4b9f705c7f622793da2b7
MahnamNauman <mnaum085@uottawa.ca>
2019-10-19 00:21:57 -0400

    addition of sign out button

--- WalkinClinic/app/src/main/java/com/example/walkinclinicv01/WelcomeWindow.java
+++ WalkinClinic/app/src/main/java/com/example/walkinclinicv01/WelcomeWindow.java
@@ -1,18 +1,34 @@
 package com.example.walkinclinicv01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeWindow extends AppCompatActivity {
 
+    Button userSignOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_window);
+
+        userSignOut = findViewById(R.id.SignOut);
+
+        userSignOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(WelcomeWindow.this, MainActivity.class));
+            }
+        }
+        );
     }
 
 }
