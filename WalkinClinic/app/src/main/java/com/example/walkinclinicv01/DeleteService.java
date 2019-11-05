3f0997788b5f015b35050225261e8045b9587d77
kayladonovan <kdono062@uottawa.ca>
2019-11-05 12:26:00 -0500

    adding screens

---
+++ WalkinClinic/app/src/main/java/com/example/walkinclinicv01/DeleteService.java
@@ -0,0 +1,14 @@
+package com.example.walkinclinicv01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_service);
+    }
+}
