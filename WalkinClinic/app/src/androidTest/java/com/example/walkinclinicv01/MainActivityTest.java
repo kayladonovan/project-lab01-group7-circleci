14e8278edd77a511da84d762a611abd2610661c5
Prash1998 <psivo052@uottawa.ca>
2019-11-10 15:21:54 -0500

    chnage to mainactivitytest b branch f/

--- WalkinClinic/app/src/androidTest/java/com/example/walkinclinicv01/MainActivityTest.java
+++ WalkinClinic/app/src/androidTest/java/com/example/walkinclinicv01/MainActivityTest.java
@@ -1,19 +1,15 @@
 package com.example.walkinclinicv01;
 
 
-
-
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.test.annotation.UiThreadTest;
 import androidx.test.rule.ActivityTestRule;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
 
 public class MainActivityTest {
 
@@ -23,34 +19,49 @@ public class MainActivityTest {
     private MainActivity myActivity=null;
     private TextView text;
     private TextView text2;
-    private Button login;
 
 
     @Before
     public void setUp() {
-        myActivity=myActivityTestRule.getActivity();
+        myActivity = myActivityTestRule.getActivity();
     }
 
     @Test
     @UiThreadTest
-
     public void adminlogin() throws Exception{
 
-        text=myActivity.findViewById(R.id.username);
+        text= myActivity.findViewById(R.id.username);
         text.setText("qwe@gmail.com");
 
         text2=myActivity.findViewById(R.id.password);
         text2.setText("123456");
 
-        String check=text.getText().toString();
-        String check2=text2.getText().toString();
+        myActivity.runOnUiThread(new Runnable() {
 
+            @Override
+            public void run() {
 
-        assertEquals("qwe@gmail.com",check);
-        assertEquals("123456",check2);
+                // Stuff that updates the UI
+                myActivity.findViewById(R.id.signIn).performClick();
 
+            }
+        });
 
+    }
 
+    @Test
+    @UiThreadTest
+    public void register() throws Exception{
+        myActivity.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                // Stuff that updates the UI
+                myActivity.findViewById(R.id.register).performClick();
+
+            }
+        });
     }
 
 }
