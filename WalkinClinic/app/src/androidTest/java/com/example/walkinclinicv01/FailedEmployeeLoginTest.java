d2cfe5e89b3463ca5b36fb631bf0fe85d2cbbc95
Prash1998 <psivo052@uottawa.ca>
2019-11-10 14:33:26 -0500

    tests for logins

---
+++ WalkinClinic/app/src/androidTest/java/com/example/walkinclinicv01/FailedEmployeeLoginTest.java
@@ -0,0 +1,55 @@
+package com.example.walkinclinicv01;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.test.annotation.UiThreadTest;
+import androidx.test.rule.ActivityTestRule;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+
+
+
+public class FailedEmployeeLoginTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> myActivityTestRule = new ActivityTestRule(MainActivity.class);
+
+    private MainActivity myActivity = null;
+    private TextView text;
+    private TextView text2;
+    private Button login;
+
+
+    @Before
+    public void setUp() {
+        myActivity = myActivityTestRule.getActivity();
+    }
+
+    @Test
+    @UiThreadTest
+
+    public void employeelogin() throws Exception {
+
+        text = myActivity.findViewById(R.id.username);
+        text.setText("ymemployeetest@gmail.com");
+
+        text2 = myActivity.findViewById(R.id.password);
+        text2.setText("test123456");
+
+        String check = text.getText().toString();
+        String check2 = text2.getText().toString();
+
+
+        assertEquals("ymemployeetest@gmail.com", check);
+        assertEquals("test123", check2);
+
+    }
+
+
+}
