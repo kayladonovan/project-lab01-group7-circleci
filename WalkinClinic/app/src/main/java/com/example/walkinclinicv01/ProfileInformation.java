cd7f15d06ac94d1a85336bd456c1897774f8d588
kayladonovan <kdono062@uottawa.ca>
2019-11-14 20:47:11 -0500

    adding clinic screens

---
+++ WalkinClinic/app/src/main/java/com/example/walkinclinicv01/ProfileInformation.java
@@ -0,0 +1,14 @@
+package com.example.walkinclinicv01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_information);
+    }
+}
