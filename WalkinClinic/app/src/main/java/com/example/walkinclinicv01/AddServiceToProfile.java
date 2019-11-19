7a266d21eb005d9047adce7a7b1c1cac5da322c2
kayladonovan <kdono062@uottawa.ca>
2019-11-19 11:48:33 -0500

    button fix and final layout fix

--- WalkinClinic/app/src/main/java/com/example/walkinclinicv01/AddServiceToProfile.java
+++ WalkinClinic/app/src/main/java/com/example/walkinclinicv01/AddServiceToProfile.java
@@ -2,13 +2,31 @@ package com.example.walkinclinicv01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class AddServiceToProfile extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AddServiceToProfile extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_service_to_profile);
+
+        findViewById(R.id.createBtn).setOnClickListener(this);
+        findViewById(R.id.cancelBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            /*case R.id.createBtn:
+                break;*/
+            case R.id.cancelBtn:
+                startActivity(new Intent(AddServiceToProfile.this, ClinicServicesScreen.class));
+                break;
+        }
     }
 }
--- WalkinClinic/app/src/main/java/com/example/walkinclinicv01/DeleteServiceFromProfile.java
+++ WalkinClinic/app/src/main/java/com/example/walkinclinicv01/DeleteServiceFromProfile.java
@@ -2,13 +2,29 @@ package com.example.walkinclinicv01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class DeleteServiceFromProfile extends AppCompatActivity {
+public class DeleteServiceFromProfile extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_service_from_profile);
+
+        findViewById(R.id.deleteBtn).setOnClickListener(this);
+        findViewById(R.id.cancelBtn).setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            /*case R.id.createBtn:
+                break;*/
+            case R.id.cancelBtn:
+                startActivity(new Intent(DeleteServiceFromProfile.this, ClinicServicesScreen.class));
+                break;
+        }}
 }
