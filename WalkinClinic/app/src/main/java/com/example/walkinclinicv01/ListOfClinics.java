b35e77df99cf18c977f39d9556a5d33dbf944060
kayladonovan <kdono062@uottawa.ca>
2019-11-23 16:12:59 -0500

    temp commit

--- WalkinClinic/app/src/main/java/com/example/walkinclinicv01/ListOfClinics.java
+++ WalkinClinic/app/src/main/java/com/example/walkinclinicv01/ListOfClinics.java
@@ -1,8 +1,10 @@
 package com.example.walkinclinicv01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,15 +23,17 @@ public class ListOfClinics extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
 
+    TextView ClinicNameTextView;
+
     DatabaseReference mClinics = myRef.child("Clinics"); //specific ref to Clinics in DB
     LinearLayout linearLayout; //needed for ref of where to put the textViews
     int numOfClinics; //counter for clinics
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_clinics);
-
         linearLayout = findViewById(R.id.linearLayout);
 
         /*Determines number of Clinics in the DB by calling getChildrenCount() and storing the value as an integer
@@ -40,35 +44,35 @@ public class ListOfClinics extends AppCompatActivity {
         //Query match = mClinics.orderByChild("").equalTo("");  //orderByChild() returns a new Query narrowed by the constraint
         mClinics.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange (DataSnapshot dataSnapshot){
+            public void onDataChange (@NonNull DataSnapshot dataSnapshot){
                 numOfClinics = (int) dataSnapshot.getChildrenCount();
-                addTextViews();
+                addSearchResults();
+
+                //Info info = dataSnapshot.child("Info").getValue(Info.class);
+                //name = info.getName();
+
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        }
-
-        );
+        });
     }
 
-
-
     //creates textViews based on numOfClinics
-    private void addTextViews() {
+    private void addSearchResults() {
         LinearLayout textLinearLayout = new LinearLayout(this);
         textLinearLayout.setOrientation(LinearLayout.VERTICAL);
 
         linearLayout.addView(textLinearLayout);
-
         int size = numOfClinics;
 
         for (int i = 1; i <= size; i++) {
-            TextView textView = new TextView(this);
-            textView.setText(" " + i);
-            textLinearLayout.addView(textView);
+            Button select = new Button(this);
+            select.setText("select"); //need to set text to clinic name
+            select.setId(i);
+            linearLayout.addView(select);
         }
     }
 }
